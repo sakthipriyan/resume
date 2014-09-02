c96cba0ba8b04f40e926a62712165b1f34f17f33
Sakthi Priyan H <sakthipriyan@crayondata.com>
2014-09-03 01:31:22 +0530

    Added all required files

---
+++ README.md
@@ -0,0 +1,2 @@
+gs -dBATCH -dNOPAUSE -sOutputFile=resume_4.0_12.pdf -sDEVICE=pdfwrite  -c "<</Orientation 3>> setpagedevice"   -f resume.ps
+
---
+++ resume.mm
@@ -0,0 +1,384 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1409391066223" ID="ID_1833024927" MODIFIED="1409683860986" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <b>Sakthi Priyan H</b>
+    </p>
+    <p style="text-align: center">
+      26+ years old
+    </p>
+    <p style="text-align: center">
+      5.2 Years Experience
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1409426700451" ID="ID_1619699193" MODIFIED="1409677528539" POSITION="right" STYLE="fork" TEXT="Objective">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1409426610675" ID="ID_1046531036" MODIFIED="1409429071522" STYLE="fork" TEXT="I want to be a part of a world class team working &#xa;on innovative and technically challenging applications &#xa;which can have profound impact on people/business. ">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1409391249814" ID="ID_466319025" MODIFIED="1409677527435" POSITION="right" STYLE="fork" TEXT="Education">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1409391260068" ID="ID_1741302267" MODIFIED="1409678418258" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>St Ann's Higher Secondary School,</b>
+    </p>
+    <p>
+      Tindivanam
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1409398519223" ID="ID_1823246046" MODIFIED="1409680780832" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      2003 X - 91.4%
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1409680767063" ID="ID_463566216" MODIFIED="1409680784282" TEXT="2005 XII - 93.75%"/>
+<node CREATED="1409427006205" ID="ID_930740247" MODIFIED="1409680747703" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      XII - Centum in Mathematics
+    </p>
+    <p>
+      and school First in English.
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1409391286517" ID="ID_1352200550" MODIFIED="1409678408487" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Coimbatore Institute of Technology,</b>
+    </p>
+    <p>
+      Coimbatore
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1409398538147" ID="ID_1360898477" MODIFIED="1409677952935" STYLE="fork" TEXT="2009 BE(ECE) - 8.03/10">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1409427006202" ID="ID_312533177" MODIFIED="1409681488517" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Participated/won various
+    </p>
+    <p>
+      inter/intra college
+    </p>
+    <p>
+      competitions.
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1409427006203" ID="ID_29020099" MODIFIED="1409681465403" STYLE="fork" TEXT="Events Manager&#xa;Interface2k8&#xa;Technical Symposium">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1409427006203" ID="ID_1650971949" MODIFIED="1409681389984" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Bosch awarded our final
+    </p>
+    <p>
+      year project,
+    </p>
+    <p>
+      Home Area Networking
+    </p>
+    <p>
+      as the best project
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1409402444977" ID="ID_320173614" MODIFIED="1409677524479" POSITION="left" STYLE="fork" TEXT="skills">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1409676627430" ID="ID_176403139" MODIFIED="1409676633265" TEXT="languages">
+<node CREATED="1409429229185" ID="ID_1470552116" MODIFIED="1409683149177" TEXT="Java">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1409676105096" ID="ID_214108457" MODIFIED="1409678177973" TEXT="Spring, Hibernate"/>
+<node CREATED="1409678059970" ID="ID_658740181" MODIFIED="1409681544752" TEXT="JSP, JSTL, JSF,RichFaces"/>
+<node CREATED="1409678179722" ID="ID_31931788" MODIFIED="1409678179722" TEXT="Junit, JMeter"/>
+</node>
+<node CREATED="1409429233182" ID="ID_1165614307" MODIFIED="1409683180595" TEXT="Scala">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1409429922632" ID="ID_630039744" MODIFIED="1409680654507" TEXT="Play Framework"/>
+<node CREATED="1409680655992" ID="ID_1130116459" MODIFIED="1409680664734" TEXT="Akka Framework"/>
+</node>
+<node CREATED="1409429237388" ID="ID_1754593311" MODIFIED="1409676710380" TEXT="nodejs, Python, PHP, linux Shell"/>
+</node>
+<node CREATED="1409429856628" ID="ID_1110913940" MODIFIED="1409683164497" TEXT="Web">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1409430039656" ID="ID_873559929" MODIFIED="1409430045834" TEXT="services">
+<node CREATED="1409430050370" ID="ID_880921821" MODIFIED="1409430061982" TEXT="Web services/XML"/>
+<node CREATED="1409430050371" ID="ID_376304081" MODIFIED="1409430068311" TEXT="RESTful services/JSON"/>
+</node>
+<node CREATED="1409676433050" ID="ID_1878051664" MODIFIED="1409676433050" TEXT="HTML, CSS, JavaScript"/>
+<node CREATED="1409678126902" ID="ID_309258082" MODIFIED="1409678157470" TEXT="AJAX, JQuery, Twitter Bootstrap"/>
+</node>
+<node CREATED="1409430204107" ID="ID_765447778" MODIFIED="1409430210256" TEXT="Bigdata">
+<node CREATED="1409677994401" ID="ID_1165225338" MODIFIED="1409677996096" TEXT="Couchbase, Elastic search, Kafka"/>
+<node CREATED="1409676238923" ID="ID_552571688" MODIFIED="1409677989767">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Hadoop Ecosystem
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1409681992425" ID="ID_1920191415" MODIFIED="1409681995194" TEXT="talend"/>
+</node>
+<node CREATED="1409429182841" ID="ID_1618380095" MODIFIED="1409429189221" TEXT="Cloud">
+<node CREATED="1409429190148" ID="ID_1206028091" MODIFIED="1409429190148" TEXT="Google App Engine"/>
+<node CREATED="1409429198802" ID="ID_1248767949" MODIFIED="1409429208217" TEXT="Amazon Web Services"/>
+</node>
+<node CREATED="1409429263409" ID="ID_985413901" MODIFIED="1409429265052" TEXT="SCM">
+<node CREATED="1409429266408" ID="ID_209723839" MODIFIED="1409676280403" TEXT="Git, SVN, Clearcase"/>
+</node>
+<node CREATED="1409429305461" ID="ID_132548737" MODIFIED="1409429306867" TEXT="IDE">
+<node CREATED="1409676377929" ID="ID_1681765897" MODIFIED="1409676377929" TEXT="Eclipse, Netbeans, RAD"/>
+</node>
+<node CREATED="1409429351424" ID="ID_826917662" MODIFIED="1409429357434" TEXT="Webserver">
+<node CREATED="1409429358677" ID="ID_54705948" MODIFIED="1409676334143" TEXT="IBM Websphere, Tomcat"/>
+<node CREATED="1409429378228" ID="ID_757587333" MODIFIED="1409676346631" TEXT="Apache Web server, Nginx"/>
+</node>
+<node CREATED="1409429434102" ID="ID_1384126433" MODIFIED="1409429436119" TEXT="Mobile">
+<node CREATED="1409429441529" ID="ID_1184136154" MODIFIED="1409676199116" TEXT="Android, Phonegap"/>
+</node>
+<node CREATED="1409429482622" ID="ID_1712420458" MODIFIED="1409429486776" TEXT="OS">
+<node CREATED="1409429490196" ID="ID_1182710328" MODIFIED="1409429490196" TEXT="Windows XP, 7"/>
+<node CREATED="1409429490197" ID="ID_412873560" MODIFIED="1409429490197" TEXT="Ubuntu 12.04"/>
+</node>
+<node CREATED="1409429532087" ID="ID_3108783" MODIFIED="1409676164714" TEXT="Build Tools">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1409676152690" ID="ID_1765932722" MODIFIED="1409679330938" TEXT="Ant, Gradle, Maven, SBT"/>
+<node CREATED="1409679333870" ID="ID_382389879" MODIFIED="1409679337887" TEXT="Jenkins"/>
+</node>
+<node CREATED="1409429758540" ID="ID_1706936291" MODIFIED="1409429893848" TEXT="Databases">
+<node CREATED="1409676313587" ID="ID_1598609577" MODIFIED="1409676313587" TEXT="Oracle, MySQL, SQLite"/>
+</node>
+<node CREATED="1409681820800" ID="ID_1939960224" MODIFIED="1409682200145" TEXT="webtools">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1409681946830" ID="ID_1478827701" MODIFIED="1409681958200" TEXT="bitbucket/github"/>
+<node CREATED="1409681958786" ID="ID_416249688" MODIFIED="1409681962288" TEXT="confluence"/>
+<node CREATED="1409681962677" ID="ID_905277459" MODIFIED="1409681967586" TEXT="jira"/>
+</node>
+<node CREATED="1409678755840" ID="ID_797644457" MODIFIED="1409684275179">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <i>Proficiency level varies.&#160;</i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1409402025073" ID="ID_105779382" MODIFIED="1409678430058" POSITION="right" STYLE="fork" TEXT="Work">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1409402039775" ID="ID_1659272030" MODIFIED="1409677515996" STYLE="fork" TEXT="Infosys Limited. 2009 - 2013">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1409426903259" ID="ID_824043258" MODIFIED="1409429071511" STYLE="fork" TEXT="Software for Insurance Systems.">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1409426903263" ID="ID_783570341" MODIFIED="1409429071512" STYLE="fork" TEXT="Software for People Systems.">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1409426903264" ID="ID_1851511837" MODIFIED="1409429071512" STYLE="fork" TEXT="Software for Retail Logistics Centre.">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1409426965322" ID="ID_1314036220" MODIFIED="1409429071512" STYLE="fork" TEXT="Secured CGPA 5.0/5.0 in Infosys Training.">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1409426965324" ID="ID_1003327807" MODIFIED="1409429071512" STYLE="fork" TEXT="Awarded Rising Star Award.">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1409402056666" ID="ID_862579346" MODIFIED="1409677519146" STYLE="fork" TEXT="Crayon data. 2013 - present">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1409427309945" ID="ID_1037423996" MODIFIED="1409677509989" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Data Acquistion Framework</b>
+    </p>
+    <p>
+      Distributed web crawler framework
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1409427322403" ID="ID_1395289365" MODIFIED="1409677578672" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Choice API</b>
+    </p>
+    <p>
+      Muti tenant application to serve choices
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1409677793457" ID="ID_265815523" MODIFIED="1409677848685">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Cebit/Maya/Yoda/Fabindia</b>
+    </p>
+    <p>
+      REST API for mobile/web apps
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1409430284415" ID="ID_1702593874" MODIFIED="1409677521406" TEXT="Hobby Projects">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1409427191983" ID="ID_786752008" MODIFIED="1409430323233" STYLE="fork" TEXT="Earlier wrote Tweetbot services for Raspberry Pi.">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1409430327320" ID="ID_1063412746" MODIFIED="1409430337521" TEXT="Android apps with GAE backend before that."/>
+<node CREATED="1409430354649" ID="ID_238286298" MODIFIED="1409430355732" TEXT="PHP web app for a Quiz event in college was the starter."/>
+</node>
+</node>
+<node CREATED="1409391171550" ID="ID_757107082" MODIFIED="1409678625666" POSITION="right" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Contact
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1409391178063" ID="ID_367898949" MODIFIED="1409678574646" STYLE="fork" TEXT="sakthipriyans@gmail.com">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1409391194550" ID="ID_530098772" MODIFIED="1409678574241" STYLE="fork" TEXT="+91 9944352635">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1409391205143" ID="ID_1392598225" MODIFIED="1409678389770" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      49, Mallipathan Street, Tindivanam,
+    </p>
+    <p>
+      Tamil Nadu, India. PIN - 604001
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1409402791477" ID="ID_8331191" MODIFIED="1409683332862" POSITION="left" STYLE="fork" TEXT="About me">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1409426829646" ID="ID_587748752" MODIFIED="1409429071522" STYLE="fork" TEXT="Quick &amp; Self Learner">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1409426829668" ID="ID_520591094" MODIFIED="1409429071522" STYLE="fork" TEXT="Good Analytical skills">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1409426829673" ID="ID_71987554" MODIFIED="1409429071522" STYLE="fork" TEXT="Can do Attitude">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1409678873937" ID="ID_1295553074" MODIFIED="1409678899681" POSITION="left" TEXT="Resume">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1409410723198" ID="ID_334948160" MODIFIED="1409678905758" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Version 4.0
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1409678649988" ID="ID_1309345168" MODIFIED="1409683259513" TEXT="2014, Sep 3"/>
+</node>
+</node>
+</map>
